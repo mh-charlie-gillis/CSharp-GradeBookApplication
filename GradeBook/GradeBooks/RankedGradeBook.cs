9feaee85d823a55b25313d39c0ec169263ddafb1
Charlie Gillis <charlie.gillis@mh.alabama.gov>
2018-04-03 08:44:28 -0500

    Implement Ranked Grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,38 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name) 
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+
+                var dropCount = (int)Math.Ceiling(Students.Count * 0.2);
+
+                var gradeList = (from student in Students
+                          orderby student.AverageGrade descending
+                          select student.AverageGrade).ToList();
+
+                if (gradeList[dropCount - 1] <= averageGrade)
+                    return 'A';
+                else if (gradeList[(dropCount * 2) - 1] <= averageGrade)
+                    return 'B';
+                else if (gradeList[(dropCount * 3) - 1] <= averageGrade)
+                    return 'C';
+                else if (gradeList[(dropCount * 4) - 1] <= averageGrade)
+                    return 'D';
+                else
+                    return 'F';
+            }
         }
     }
 }
